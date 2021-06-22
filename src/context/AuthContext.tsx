db5aa7d0edd9eb4058b2e6eda27c942170f1978c
leticia-gm <leticiagmagretti@gmail.com>
2021-06-22 11:15:15 -0300

    primeiro commit

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,68 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { auth, firebase } from '../services/firebase';
+
+type Persona = {
+    id: string;
+    name: string;
+    avatar: string;
+}
+
+type AuthContextType = {
+    persona: Persona | undefined;
+    signInWithGoogle: () => Promise<void>;
+}
+
+type AuthContextProviderProps = {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextType);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+    const [persona, setPersona] = useState<Persona>();
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(persona => {
+            if (persona) {
+                const { displayName, photoURL, uid } = persona
+
+                if (!displayName || !photoURL) {
+                    throw new Error('Há informações da conta Google faltando.');
+                }
+
+                setPersona({
+                    id: uid,
+                    name: displayName,
+                    avatar: photoURL
+                });
+            }
+        })
+
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+    async function signInWithGoogle() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        const result = await auth.signInWithPopup(provider);
+
+        if (result.user) {
+            const { displayName, photoURL, uid } = result.user
+            if (!displayName || !photoURL) {
+                throw new Error('Há informações da conta Google faltando.');
+            }
+
+            setPersona({
+                id: uid,
+                name: displayName,
+                avatar: photoURL
+            });
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{ persona, signInWithGoogle }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import '../styles/auth.scss'
+import { useAuth } from '../hooks/useAuth';
+
+export function NewRoom() {
+    const { persona } = useAuth();
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Let Me Ask" />
+                <strong>Crie salas de Q&amp;A ao vivo</strong>
+                <p>Tire as suas dúvidas em tempo real.</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <h2>Crie uma nova sala</h2>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Nome da sala"
+                        />
+                        <Button type="submit">
+                            Criar sala
+                        </Button>
+                    </form>
+                    <p>Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link></p>
+                </div>
+            </main >
+        </div >
+    )
+}
